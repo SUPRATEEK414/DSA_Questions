d379661d923f2163b7926c8ad4ed63c7901c2ede
Suprateek Sarkar <81064383+SUPRATEEK414@users.noreply.github.com>
2024-05-08 08:10:57 +0530

    Add files via upload

---
+++ Lecture 3 If-Else, While loop & Lots of Patterns (Part-1).cpp
@@ -0,0 +1,203 @@
+#include <iostream>
+using namespace std;
+int main() {
+  //----------------------------------------------- Part 1
+  // int a;
+  // cin>>a;
+  // if(a>0){
+  //   cout<< "a is positive" << endl;
+  // }
+  // else{
+  //   cout<< "a is negative" << endl;
+  // }
+
+  //----------------------------------------------- Part 2
+  // int a, b;
+  // cout << "Enter the value of a " << endl;
+  // cin>> a;
+  // cout << "Enter the value of b " << endl;
+  // cin>> b;
+  // if(a>b){
+  //   cout << "A is greater"<< endl;
+  // }
+  // if(a<b){
+  //   cout << "B is greater"<< endl;
+  // }
+
+  //----------------------------------------------- Part 3
+  // int a;
+  // cout<<"Enter the value of a "<<endl;
+  // cin>> a;
+  // if(a>0){
+  //   cout<<"A is positive"<<endl;
+  // }
+  // else
+  // {
+  //   if(a<0){
+  //     cout<<"A is negative"<<endl;
+  //   }
+  //   else
+  //   {
+  //     cout<<"A is zero"<<endl;
+  //   }
+  // }
+
+  //----------------------------------------------- Part 4
+  // int a;
+  // cout<<"Enter the value of a "<<endl;
+  // cin>> a;
+  // if(a>0){
+  //   cout<<"A is positive"<<endl;
+  // }
+  // else if (a<0)
+  // {
+  //   cout<<"A is negative"<<endl;
+  // }
+  // else
+  //   {
+  //     cout<<"A is zero"<<endl;
+  //   }
+
+  //----------------------------------------------- Part 5
+  // int a;
+  // cout<<"Enter the value of a"<<endl;
+  // cin>> a;
+
+  // if(a==9){
+  //   cout<<"NINEY"<<endl;
+  // }
+  // else if (a>0){
+  //   cout<<"POSITIVE"<<endl;
+  // }
+  // else
+  // {
+  //   cout<<"NEGATIVE"<<endl;
+  // }
+
+  //----------------------------------------------- Part 6
+  // int a, b;
+  // cout<<"enter the value of a and b"<<endl;
+  // cin>>a>>b;
+  // b = a + 1;
+  // if ((a)==b){
+  //   cout<<a<<endl;
+  // }
+  // else
+  // {
+  //   cout<< a + 1<<endl;
+  // }
+
+  //----------------------------------------------- Part 7
+  // int a;
+  // cout<<"enter the value of a"<<endl;
+  // cin>>a;
+  // if(a>20){
+  //   cout<<"Love"<<endl;
+  // }
+  // else if(a==24){
+  //   cout<<"Lovely"<<endl;
+  // }
+  // else{
+  //   cout<<"Suprateek"<<endl;
+  // }
+
+  //----------------------------------------------- Part 8
+  // char ch;
+  // cout << "Enter the character value : ";
+  // cin >> ch;
+
+  // if (ch >= 'a' && ch <= 'z') {
+  //   cout << "The given character is Lower Case" << endl;
+  // } else if (ch >= 'A' && ch <= 'Z') {
+  //   cout << "The given character is Upper Case" << endl;
+  // } else {
+  //   cout << "The given value is Numeric" << endl;
+  // }
+
+  //----------------------------------------------- WHILE LOOP
+  // Print (1 to N)
+  // int n;
+  // cout<<"Enter the value of n: ";
+  // cin>>n;
+  // int i=1;
+  // while(i<=n){
+  //   cout<<i<<" ";
+  //   i=i+1;
+  // }
+
+  // Print sum(1 to N)
+  //  int n;
+  //  cout<<"Enter the value of n: ";
+  //  cin>>n;
+  //  int i=1;
+  //  int sum=0;
+  //  while(i<=n){
+  //    sum=sum+i;
+  //    i=i+1;
+  //  }
+  //  cout<<"value of sum is "<<sum<<endl;
+
+  // find sum of all even number
+  // int n;
+  // cout<<"Enter the value of n : ";
+  // cin>>n;
+  // int sum = 0;
+  // int i = 2;
+
+  //  while(i <= n){
+  //    sum = sum + i;
+  //    i = i + 2;
+  //  }
+  //  cout<<"Sum of all even numbers : "<< sum<< endl;
+  //  return 0;
+
+  // farenheit to celcius conversion
+  // int f, c;
+  // cin>>f;
+  // c=5*(f-32)/9;
+  // cout<<c<<"degree C"<<endl;
+
+  // prime or not
+  // int n;
+  // cout << "Enter the value of n: ";
+  // cin >> n;
+  // int i = 2;
+  // while (i < n) {
+  //   if (n % i == 0) {
+  //     cout << "Not Prime" << endl;
+  //   }
+  //   else{
+  //     cout<< "prime for "<<i<<endl;
+  //   }
+  //   i = i + 1;
+  // }
+
+  // PATTERN based problems
+  // pattern 1
+  //  int n;
+  //  cin>>n;
+  //  int i = 1;
+  //  while(i <= n){
+  //    int j = 1;
+  //    while(j <= n){
+  //      cout<<"*";
+  //      j = j + 1;
+  //    }
+  //    cout<<endl;
+  //    i = i + 1;
+  //  }
+
+  // pattern 2
+  //  int n;
+  //  cin>>n;
+  //  int i = 1;
+  //  while(i <= n){
+  //    int j = 1;
+  //    while(j <= n){
+  //      cout<<i;
+  //      j = j + 1;
+  //    }
+  //    cout<<endl;
+  //    i = i + 1;
+  //  }
+}
