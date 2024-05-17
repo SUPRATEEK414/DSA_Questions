ca09d0cc3c0c33ef28fd1df447abd801aa680450
Suprateek Sarkar <81064383+SUPRATEEK414@users.noreply.github.com>
2024-05-17 14:13:37 +0530

    Add files via upload

---
+++ Lecture 17 BUBBLE SORT in 1 Video Theory Optimised Code BestWorst Case Complexity.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void bubbleSort(vector<int>& arr, int n){
+  for(int i=0; i<n; i++){
+    bool swapped = false;
+    for(int j=0; j<n-i-1; j++){
+      if(arr[j]>arr[j+1]){
+        swap(arr[j], arr[j+1]);
+        swapped = true;
+      }
+    }
+    if(swapped == false){
+      break;
+    }
+  }
+}
