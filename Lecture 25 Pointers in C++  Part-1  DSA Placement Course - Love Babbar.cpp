5db18be06e24616cb55b5b20dce59e5f59e62ec1
Suprateek Sarkar <81064383+SUPRATEEK414@users.noreply.github.com>
2024-05-27 00:25:08 +0530

    Add files via upload

---
+++ Lecture 25 Pointers in C++  Part-1  DSA Placement Course - Love Babbar.cpp
@@ -0,0 +1,69 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   int num = 5;
+//   cout<<num<<endl;
+
+//   // address of Operator
+//   //cout<<"address of operator is "<<&num<<endl;
+
+//   int *ptr = &num;
+//   cout<<"Address is: "<<ptr<<endl;
+//   cout<<"value is: "<<*ptr<<endl;
+
+//   double d =4.3;
+//   double *p2 = &d;
+//   cout<<"Address is: "<<p2<<endl;
+//   cout<<"value is: "<<*p2<<endl;
+  
+//   cout<<"Size of integer is "<<sizeof(num)<<endl;
+//   cout<<"Size of pointer is "<<sizeof(ptr)<<endl;
+//   cout<<"Size of pointer is "<<sizeof(p2)<<endl;
+//   return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main(){
+//   //pointer to int is created, and pointing to some garbage address
+//   // int *p = 0;
+//   // cout<<*p<<endl;
+  
+//   // int i=5;
+//   // //int *p = &i;
+
+//   // int *p = 0;
+//   // p = &i;
+//   // cout<<p<<endl;
+//   // cout<<*p<<endl;
+
+//   int num = 5;
+//   int a = num;
+//   cout<<"a before "<<num<<endl;
+//   a++;
+//   cout<<"a after "<<num<<endl;
+
+//   cout<<num<<endl;
+
+//   int *p = &num;
+//   cout<<"before "<<num<<endl;
+//   (*p)++;
+//   cout<<"after "<<num<<endl;
+
+//   //copying a pointer
+//   int *q = p;
+//   cout<<p<<" - "<<q<<endl;
+//   cout<<*p<<" - "<<*q<<endl;
+
+//   //important concept
+//   int i = 3;
+//   int *t = &i;
+//   // cout<<(*t)++<<endl;
+//   *t = *t + 1;
+//   cout<<*t<<endl;
+//   cout<<"before t "<<t<<endl;
+//   t = t + 1;
+//   cout<<"after t "<<t<<endl;
+  
+//   return 0;
+// }
